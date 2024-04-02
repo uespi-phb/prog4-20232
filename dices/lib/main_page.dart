0c75062ebaa84404b15bdd254e7c5b3abeb7a791
Eyder Rios <eyder.rios@gmail.com>
2024-04-02 11:24:54 -0300

    Aula 2024-04-02 dices (2 dados)

--- dices/lib/main_page.dart
+++ dices/lib/main_page.dart
@@ -1,56 +1,21 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
-class MainPage extends StatefulWidget {
+import './dices_widget.dart';
+
+class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  final _rnd = Random();
-
-  int _leftDice = 1;
-  int _rightDice = 4;
-
-  void _rollDices() {
-    setState(() {
-      _leftDice = _rnd.nextInt(6) + 1;
-      _rightDice = _rnd.nextInt(6) + 1;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    debugPrint('**** build()');
+    debugPrint('MainPage.build()');
 
     return Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(
         title: const Text('Rolando Dados'),
       ),
-      body: GestureDetector(
-        onTap: _rollDices,
-        child: Center(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Image.asset('assets/images/dice$_leftDice.png'),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Image.asset('assets/images/dice$_rightDice.png'),
-                ),
-              ),
-            ],
-          ),
-        ),
+      body: const Center(
+        child: DicesWidget(),
       ),
     );
   }
